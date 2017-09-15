afbca36c9b78b9e21c213ca5af606c76d5bfb155
Dion Buckley <dion-b@hotmail.com>
2017-09-15 16:51:10 +0100

    Tidying things up, setting up screens, game loop needs to come next!

--- TextMissile/main.cpp
+++ TextMissile/main.cpp
@@ -2,18 +2,11 @@
 /// Dion Buckley
 /// C00220868
 /// Missile Text Based Game
-/// Select warhead, Acquire target, Launch code, Arm Missile, Collion ? Valid ? 
-/// 45 mins into #96
+/// Select warhead, Acquire target, Launch code, Arm Missile, Collison ? Valid ? 
 /// </summary>
 #include <iostream>
-
-enum Screen
-{
-	Main,
-	Select,
-	Acquire,
-	Launch
-};
+#include <Windows.h>
+#include <time.h>
 
 typedef struct Position
 {
@@ -30,65 +23,14 @@ enum WarHead { EXPLOSIVE, NUCLEAR };
 typedef struct Enemy {
 	Coordinates coordinates;
 }Target;
-struct Game {
-	Screen currScr;
-	int choice = 0;
-	void mainScr()
-	{
-		std::cout << "1. Select War Head" << std::endl;
-		std::cout << "2. Check Intel" << std::endl;
-		std::cout << "3. Launch Missile" << std::endl;
-		std::cout << "4. Quit" << std::endl;
-		std::cin >> choice;
-		switch (choice)
-		{
-		case 1:
-			selectScr();
-			break;
-		case 2:
-			intelScr();
-			break;
-		default:
-			break;
-		}
-	}
-	void selectScr()
-	{
-		std::cout << "1. Explosive" << std::endl;
-		std::cout << "2. Nuclear" << std::endl;
-		std::cin >> choice;
-		switch (choice)
-		{
-		case 1:
-			explosive();
-			break;
-		case 2:
-			nuclear();
-			break;
-		default:
-			break;
-		}
-	}
-	void explosive()
-	{
-	}
-	void nuclear()
-	{
-	}
-	void intelScr()
-	{
-		int counter = 0;
-		//doing cmd window timer here for intel
-	}
 
-};
 struct Missile {
 	WarHead payload;
 	Coordinates coordinates;
 	Target target;
 
 
-	bool isArmed;
+	bool isArmed = false;
 
 	void arm()
 	{
@@ -107,11 +49,101 @@ struct Missile {
 		coordinates.y += 1;
 	}
 };
+struct Game {
+	Missile missile;
+	int choice = 0;
+	int intelCounter = 20;
+	void mainScr()
+	{
+		std::cout << "1. Select War Head" << std::endl;
+		std::cout << "2. Check Intel" << std::endl;
+		std::cout << "3. Launch Missile" << std::endl;
+		std::cout << "4. Quit" << std::endl;
+		std::cin >> choice;
+		system("CLS");
+		switch (choice)
+		{
+		case 1:
+			selectScr();
+			break;
+		case 2:
+			intelScr();
+			break;
+		case 3:
+			launchScr();
+			break;
+		case 4:
+			system("exit");
+		default:
+			break;
+		}
+	}
+	void selectScr()
+	{
+		std::cout << "1. Explosive" << std::endl;
+		std::cout << "2. Nuclear" << std::endl;
+		std::cin >> choice;
+		missile.arm();
+		system("CLS");
+		switch (choice)
+		{
+		case 1:
+			explosive();
+			break;
+		case 2:
+			nuclear();
+			break;
+		default:
+			break;
+		}
+		mainScr();
+	}
+	void intelScr()
+	{
+
+		std::cout << "Current Intel will be valid for: " <<intelCounter << " seconds." << std::endl;
+		
+		//doing cmd window timer here for intel
+	}
+	void launchScr()
+	{
+		if (missile.isArmed)
+		{
+			std::cout << "Launch Code: ****\nLaunching Missile!" << std::endl;
+		}
+		else
+		{
+			std::cout << "Please select, and arm, a War Head first" << std::endl;
+			system("pause");
+			system("CLS");
+			mainScr();
+		}
+	}
+	void explosive()
+	{
+	}
+	void nuclear()
+	{
+	}
+	void update()
+	{
+		if (intelCounter > 0)
+		{
+			Sleep(1000);
+			intelCounter--;
+		}
+		else
+		{
+			intelCounter = 20;
+		}
+	}
+};
 int main()
 {
+	clock_t time;
+	time = clock();
 	Game game;
 	game.mainScr();
-	std::system("pause");
 	return 1;
 }
 
